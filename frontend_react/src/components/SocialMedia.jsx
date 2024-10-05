52f9740045f4fe7772574d3231f196df8a222819
calibrixadv <andrei.iordache21@yahoo.com>
2024-10-05 11:57:15 +0300

    FrontEnd

---
+++ frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {BsTwitter, BsInstagram} from 'react-icons/bs';
+import {FaFacebookF} from 'react-icons/fa';
+
+const SocialMedia = () => {
+  return (
+    <div className='app__social'>
+        <a href="https://www.instagram.com/calibrixadv/" target='_blank'>
+            <BsInstagram/>
+        </a>
+    </div>
+  )
+}
+
+export default SocialMedia
